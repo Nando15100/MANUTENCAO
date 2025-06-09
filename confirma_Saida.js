0f5fd2ca6aba29923079723a6e94555999a26ed2
Nando15100 <fernando.202111208@unilasalle.edu.br>
2025-06-08 21:59:21 -0300

    Create confirma_Saida.js
    FUNÇÃO QUE FAZ PARTE DO ARQUIVO  manutencao.html, comando em javascript

---
+++ confirma_Saida.js
@@ -0,0 +1,5 @@
+ function confirmarSaida() {
+      return confirm('Tem certeza que deseja sair e voltar à página inicial?');
+    }
+
+//FUNÇÃO QUE FAZ PARTE DO ARQUIVO  manutencao.html, comando em javascript
